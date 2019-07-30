0d32f8b74244971a1bbcac99c71ce9991a51b6e8
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2019-07-30 11:53:24 +0800

    merged necessary part

--- Python/DL/lab2/pairwise_jointed_distributions.py
+++ Python/DL/lab2/pairwise_jointed_distributions.py
@@ -2,7 +2,24 @@
 import os
 import matplotlib.pyplot as plot
 import seaborn
+import pandas
 
+interested_data = pandas.read_csv (
+        'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', 
+        header = None
+)
+
+interested_data.columns = [
+        'Class label', 'Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash',
+        'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols',
+        'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines',
+        'Prolinee'
+]
+
+X = interested_data.drop ('Class label', 1)
+Y = interested_data['Class label']
+
+print (interested_data.head ())
 seaborn.set (style = 'whitegrid', context = 'notebook')
 
 seaborn.pairplot ()
